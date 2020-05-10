d6072424dfdb91c7c76032330631a9dbcf537ee7
b19jenda <b19jenda@student.his.se>
2020-05-10 21:33:45 +0200

    La till menu, la till så när man klickar på menu alternativet refresh så tillkallar den nya klassen för att hämta datan.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -6,6 +6,15 @@ import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,6 +23,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,54 +34,80 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.game_menu, menu);
+        return true;
+    }
 
-@SuppressLint("StaticFieldLeak")
-private class JsonTask extends AsyncTask<String, String, String> {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_settings){
+            return true;
+        }
+        if(id == R.id.action_refresh){
+            new JsonTask().execute();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private class JsonTask extends AsyncTask<String, String, String> {
 
-    private HttpURLConnection connection = null;
-    private BufferedReader reader = null;
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
 
-    protected String doInBackground(String... params) {
-        try {
-            URL url = new URL("https://wwwlab.iit.his.se/brom/kurser/mobilprog/jsonservice.php");
-            connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-
-            InputStream stream = connection.getInputStream();
-            reader = new BufferedReader(new InputStreamReader(stream));
-
-            StringBuilder builder = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null && !isCancelled()) {
-                builder.append(line).append("\n");
-            }
-            return builder.toString();
-        } catch (MalformedURLException e) {
-
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (connection != null) {
-                connection.disconnect();
-            }
+        protected String doInBackground(String... params) {
             try {
-                if (reader != null) {
-                    reader.close();
+                URL url = new URL("https://wwwlab.iit.his.se/brom/kurser/mobilprog/jsonservice.php");
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
                 }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+
+                e.printStackTrace();
             } catch (IOException e) {
                 e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("hej","Data:"+s);
+            try {
+                JSONObject json1 = new JSONObject(s);
+                //Log.d("hej","Get:"+json1);
+                JSONArray a = json1.getJSONArray("object");
+
+               // String age = json1.getString("name");
+
+            } catch (JSONException e) {
+                Log.e("brom","E:"+e.getMessage());
             }
         }
-        return null;
-    }
-
-    private boolean isCancelled() {
-    }
-
-    @Override
-    protected void onPostExecute(String json) {
-        Log.d("TAG", json);
     }
 }
+
